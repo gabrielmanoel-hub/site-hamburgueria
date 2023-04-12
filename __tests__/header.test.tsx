fdc6f88382b32a523418b8b7e6571f255d3ad68d
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2023-04-12 17:11:44 -0300

    corrigindo erro de  Falta a propriedade src necessária na imagem

--- __tests__/header.test.tsx
+++ __tests__/header.test.tsx
@@ -2,19 +2,45 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Header from '../pages/header'
 
-describe('Header', () => {
-    it('test header', () => {
-      const {container} = render(<Header/>)
-      // const imgs = container.querySelector('image')
-      const ppp = container.hasAttribute('logo')
-      const p = container.hasAttribute('priority')
-      
-      // expect(imgs)
-      expect(ppp).toBeEmptyDOMElement()
-      expect(p)
-     
-    });
 
+interface Props {
+  src: string,
+  alt: string
+}
+
+jest.mock('next/image', () => {
+  return ({ src, alt }: Props) => {
+    return <img src={src} alt={alt} />;
+  };
+});
+
+describe('Header component', () => {
+  it('should render logo and priority elements', () => {
+    
+    const {container} = render(<Header />)
+    const header = container.getElementsByClassName('header')[0]
+    const Class = header.getAttribute('class')
+      
+      expect(Class).toBe("header")
+    
+  });
   
-})
+  it('checks if an <a> element with href="/" attribute is present <header> element.', () => {
+    const {container} = render(<Header/>)
+    expect(container.querySelector('header > a[href="/"]')).toBeTruthy()
+
+      
+  });
+
+  it("renders an image", () => {
+    const  { container } = render(<Header />);
+    const img = container.querySelector('img')
+    const alt = img?.getAttribute('alt')
+    expect(alt).toBe('logo');
+
+ });  
+});
+
+
+
 
