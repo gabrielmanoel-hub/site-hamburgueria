e4efa3ee61fe54094366036f0b892479596db223
gabrielmanoel-hub <gabrielmanoel1401@outlook.com>
2023-04-13 17:44:11 -0300

    first commit

--- __mocks__/fileMock.ts
+++ __mocks__/fileMock.ts
@@ -4,3 +4,10 @@ module.exports = {
     width: 24,
     blurDataURL: 'data:image/png;base64,imagedata',
   }
+
+  /**
+   * src: uma string que representa o caminho da imagem SVG.
+   * height: um número que representa a altura da imagem.
+   * width: um número que representa a largura da imagem.
+   * blurDataURL: uma string que representa uma imagem em formato base64. Essa imagem é usada como uma versão embaçada da imagem SVG original antes que ela seja totalmente carregada, para melhorar a experiência do usuário.
+   */
